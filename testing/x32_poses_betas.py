89007debc3537b3b34cccbd68b8083548120e6bc
finuxs <a.romeromerida@gmail.com>
2019-02-26 17:54:18 +0100

    Corrected HDF5 Generation. Now in Python2 Format

--- testing/x32_poses_betas.py
+++ testing/x32_poses_betas.py
@@ -18,8 +18,8 @@ def print_attrs(name, obj):
 DETC_PATH="/media/finuxs/HDD/H36M/H36M_skeleton/detections/"
 SHAPE_PATH="/media/finuxs/HDD/H36M/H36M_shape/data/"
 FILE_PATH="/media/finuxs/HDD/"
-SAVE_PATH="/home/finuxs/Desktop"
-subjects = [[1,5,6,7,8], [9]] #11 Missing
+SAVE_PATH="/home/finuxs/Shape-Motion-Predictor/datasets/h36m/"
+subjects = [[1,5,6,7,8], [9,11]] #11 Missing
 actions = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 subactions = [1,2]
 cameras = [1,2,3,4]
@@ -43,22 +43,22 @@ for split in range(2):
                 print(folder_name_no_cam)
                 for camera in cameras:
                     folder_name = 'S{:02d}/Act{:02d}/Subact{:02d}/Cam{:02d}/'.format(subject, action, subaction, camera)
-
-                if split == 0:
-                    tmp = f["train/skeleton/"+folder_name_no_cam].get('x32').value
-                    g['{:03d}'.format(cnt)+"/x32"]=tmp
-                    tmp = f["train/skeleton/"+folder_name_no_cam].get('betas').value
-                    g['{:03d}'.format(cnt)+"/betas"]=tmp
-                    tmp = f["train/skeleton/"+folder_name_no_cam].get('pose').value
-                    g['{:03d}'.format(cnt)+"/pose"]=tmp
-                else:
-                    tmp = f["test/skeleton/"+folder_name_no_cam].get('x32').value
-                    g['{:03d}'.format(cnt)+"/x32"]=tmp
-                    tmp = f["test/skeleton/"+folder_name_no_cam].get('betas').value
-                    g['{:03d}'.format(cnt)+"/betas"]=tmp
-                    tmp = f["test/skeleton/"+folder_name_no_cam].get('pose').value
-                    g['{:03d}'.format(cnt)+"/pose"]=tmp
-                cnt = cnt + 1
+                if not (action==2 and subject==11):
+                    if split == 0:
+                        tmp = f["train/skeleton/"+folder_name_no_cam].get('x32').value
+                        g['{:03d}'.format(cnt)+"/x32"]=tmp
+                        tmp = f["train/skeleton/"+folder_name_no_cam].get('betas').value
+                        g['{:03d}'.format(cnt)+"/betas"]=tmp
+                        tmp = f["train/skeleton/"+folder_name_no_cam].get('pose').value
+                        g['{:03d}'.format(cnt)+"/pose"]=tmp
+                    else:
+                        tmp = f["test/skeleton/"+folder_name_no_cam].get('x32').value
+                        g['{:03d}'.format(cnt)+"/x32"]=tmp
+                        tmp = f["test/skeleton/"+folder_name_no_cam].get('betas').value
+                        g['{:03d}'.format(cnt)+"/betas"]=tmp
+                        tmp = f["test/skeleton/"+folder_name_no_cam].get('pose').value
+                        g['{:03d}'.format(cnt)+"/pose"]=tmp
+                    cnt = cnt + 1
 
 print(list(g.keys()))
 f.close()
